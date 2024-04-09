f4b253879e798c49bb066a0db364f1f319056085
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-04-09 12:30:50 +1000

    create github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/github-users-browser-react/",
   plugins: [react()],
   css: {
     modules: {
