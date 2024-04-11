c395aa63f43459b74de7c963a99e8f8a35570144
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-04-11 19:33:37 +1000

    fix input component

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -1,19 +1,57 @@
 import styles from "./input.module.scss";
 import { FC } from "react";
-import { clsx } from "clsx";
-
+import TextField from "@mui/material/TextField";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 type Input = {
   type?: "text" | "password" | "radio" | "checkbox" | "hidden" | "button" | "image" | "reset" | "file" | "submit";
   placeholder?: string;
   value?: string;
 };
 
-export const Input: FC<Input> = ({ type, placeholder, value }) => {
+const theme = createTheme({
+  components: {
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: "#ffff",
+          fontFamily: "-apple-system, BlinkMacSystemFont, Segoe UI, Noto Sans, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji",
+          borderColor: "#ffff",
+          "&.Mui-focused ": {
+            color: "#ffff",
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          color: "#ffff",
+          borderColor: "#ffff",
+          ".MuiOutlinedInput-notchedOutline": {
+            borderColor: "#ffff",
+            borderWidth: "1px",
+          },
+
+          "&:hover .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#ffff",
+            borderWidth: "1px",
+          },
+
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: "#ffff",
+            borderWidth: "2px",
+          },
+        },
+      },
+    },
+  },
+});
+export const Input: FC<Input> = () => {
   return (
     <>
-      <div className={styles.wrapper}>
-        <input type={type} className={clsx(styles.input, styles[`input-${type}`])} placeholder={placeholder} value={value} />
-      </div>
+      <ThemeProvider theme={theme}>
+        <TextField id="outlined-search" label="Search field" type="search" inputProps={{ className: styles.input }} />
+      </ThemeProvider>
     </>
   );
 };
--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import clsx from "clsx";
 import styles from "./header.module.scss";
-import { Image } from "@atoms";
+import { Image, Input } from "@atoms";
 import { Navbar } from "@organisms";
 
 export const Header: FC = () => {
@@ -10,6 +10,7 @@ export const Header: FC = () => {
       <header className={clsx(styles.header)}>
         <Image src={`./icons/github-purple-icon.svg`} alt={"logo"} className={"logo"} />
         <Navbar />
+        <Input />
       </header>
     </>
   );
