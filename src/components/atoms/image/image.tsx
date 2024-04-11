604e20c99e3138dfcb2fe9ed2c26373b27294b74
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-04-11 16:10:33 +1000

    Fix styles

--- src/components/atoms/image/image.tsx
+++ src/components/atoms/image/image.tsx
@@ -7,6 +7,7 @@ type Image = {
   alt: string;
   className?: string;
 };
+
 export const Image: FC<Image> = ({ src, alt, className }) => {
   return <img src={src} alt={alt} className={clsx(styles[`${className}`])} />;
 };
--- src/components/organisms/navbar/navbar.tsx
+++ src/components/organisms/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import clsx from "clsx";
 import styles from "./navbar.module.scss";
-import { Button, Input, Link } from "@atoms";
+import { Button, Link } from "@atoms";
 
 export const Navbar: FC = () => {
   return (
@@ -10,7 +10,6 @@ export const Navbar: FC = () => {
         <Link className={"link"} href={"https://github.com/"} textContent={"GitHub"} target={"__blank"} />
         <Link className={"link"} href={"https://docs.github.com/ru/rest?apiVersion=2022-11-28"} textContent={"GitHub API"} target={"__blank"} />
         <Button type={"button"} textContent={"Users"} />
-        <Input type={"text"} placeholder={"Find user"} />
       </nav>
     </>
   );
