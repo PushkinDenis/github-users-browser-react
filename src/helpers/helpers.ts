bc4b9368cb82b12149d72fe40f67c2ac937ed9cf
PushkinDenis <105781316+PushkinDenis@users.noreply.github.com>
2024-04-22 14:57:08 +1000

    Update helpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,22 +1,4 @@
-import { UserCardProps } from "@organisms";
-
 export const fetchData = async (usersPerPage: string) => {
   const response = await fetch(`https://api.github.com/users?since=0&per_page=${usersPerPage}`);
   return await response.json();
-};
-
-export const createCard = async () => {
-  const userArr = [];
-  const users = await fetchData("100");
-  for (const user of users) {
-    const gitUser: UserCardProps = {
-      login: user.login,
-      id: user.id,
-      url: user.html_url,
-      avatar_url: user["avatar_url"],
-    };
-    userArr.push(gitUser);
-  }
-  console.log(userArr);
-  return userArr;
 };
