a095e2934ade80e5f7a04e5960ccd4ea4c89d2b0
Denis <denisp9831@gmail.com>
2024-05-08 15:13:15 +1000

    responsive input fix

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -3,14 +3,29 @@ import clsx from "clsx";
 import styles from "./header.module.scss";
 import { Image, Input } from "@atoms";
 import { Navbar } from "@organisms";
+import { useMediaQuery } from "react-responsive";
 
 export const Header: FC = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
   return (
     <>
       <header className={clsx(styles.header)}>
-        <Image src={`./icons/github-purple-icon.svg`} alt={"logo"} className={"logo"} />
-        <Navbar />
-        <Input />
+        {isDesktopOrLaptop && (
+          <>
+            <Image src={`./icons/github-purple-icon.svg`} alt={"logo"} className={"logo"} />
+            <Navbar />
+            <Input />
+          </>
+        )}
+        {isTabletOrMobile && (
+          <>
+            <Image src={`./icons/github-purple-icon.svg`} alt={"logo"} className={"logo"} />
+            <Navbar />
+          </>
+        )}
       </header>
     </>
   );
