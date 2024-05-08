068900638d2a5698fd1b5259978ee6f0a11c2824
Denis <denisp9831@gmail.com>
2024-05-08 14:07:41 +1000

    add responsive designe

--- src/components/organisms/navbar/navbar.tsx
+++ src/components/organisms/navbar/navbar.tsx
@@ -2,14 +2,24 @@ import { FC } from "react";
 import clsx from "clsx";
 import styles from "./navbar.module.scss";
 import { Button, Link } from "@atoms";
+import { useMediaQuery } from "react-responsive";
 
 export const Navbar: FC = () => {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
   return (
     <>
       <nav className={clsx(styles.navbar)}>
-        <Link className={"link"} href={"https://github.com/"} textContent={"GitHub"} target={"__blank"} />
-        <Link className={"link"} href={"https://docs.github.com/ru/rest?apiVersion=2022-11-28"} textContent={"GitHub API"} target={"__blank"} />
-        <Button type={"button"} textContent={"Users"} />
+        {isTabletOrMobile && <Button type={"button"} textContent={"Users"} />}
+        {isDesktopOrLaptop && (
+          <>
+            <Link className={"link"} href={"https://github.com/"} textContent={"GitHub"} target={"__blank"} />
+            <Link className={"link"} href={"https://docs.github.com/ru/rest?apiVersion=2022-11-28"} textContent={"GitHub API"} target={"__blank"} />
+            <Button type={"button"} textContent={"Users"} />
+          </>
+        )}
       </nav>
     </>
   );
