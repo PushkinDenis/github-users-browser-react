6dd6d5e17a44b067d24b65219c3885260b2a1762
Denis <denisp9831@gmail.com>
2024-05-13 14:01:38 +1000

    debounce added

--- src/components/atoms/input/input.tsx
+++ src/components/atoms/input/input.tsx
@@ -1,8 +1,9 @@
 import styles from "./input.module.scss";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "../../../../variables.scss";
+import { useDebounce } from "use-debounce";
 
 type InputProps = {
   type?: "text" | "password" | "radio" | "checkbox" | "hidden" | "button" | "image" | "reset" | "file" | "submit";
@@ -54,12 +55,49 @@ const theme = createTheme({
   },
 });
 
+type UserType = {
+  login: string;
+};
+
 export const Input: FC<InputProps> = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [result, setResult] = useState<any>([]);
+
+  const [debounced] = useDebounce(searchTerm, 1000);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://api.github.com/users/${debounced}`);
+        const json = await response.json();
+        setResult(json);
+        console.log(result);
+      } catch (error) {
+        console.error("Error fetching", error);
+      }
+    };
+    if (debounced) {
+      fetchData();
+    } else {
+      setResult([]);
+    }
+  }, [debounced]);
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <TextField id="outlined-search" label="Search" type="search" inputProps={{ className: styles.input }} />
+        <TextField id="outlined-search" label="Search" type="search" value={searchTerm} inputProps={{ className: styles.input }} onChange={handleInputChange} />
       </ThemeProvider>
+      {result && (
+        <>
+          <div>{result.login}</div>
+          <div>{result.html_url}</div>
+        </>
+      )}
     </>
   );
 };
--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -2,3 +2,9 @@ export const fetchData = async (usersPerPage: string, page: string) => {
   const response = await fetch(`https://api.github.com/users?since=${page}&per_page=${usersPerPage}`);
   return await response.json();
 };
+
+export const fetchUsers = async (user: string) => {
+  const response = await (await fetch(`https://api.github.com/users/${user}`)).json();
+  console.log(response);
+  return response;
+};
